c773bf0d205f1f9b260acf6dc840008052a0743c
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-09-20 19:30:50 +0500

    добавлены массивы arr1, arr2  для примера

--- task/Program.cs
+++ task/Program.cs
@@ -1,6 +1,9 @@
 ﻿//Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 string[] arr = new string[] { "12345", "-32", "hi", "world", "helloWorld" };
+string[] arr1 = new string[] { ")))", "-32", "hi", "21", "helloWorld" };
+string[] arr2 = new string[] { "12345", "-832", "hhhi", "world", "helloWorld" };
+
 
 string[] SortArray(string[] array)
 {
@@ -30,3 +33,9 @@ void PrintArray(string[] array)
 
 PrintArray(arr);
 PrintArray(SortArray(arr));
+
+PrintArray(arr1);
+PrintArray(SortArray(arr1));
+
+PrintArray(arr2);
+PrintArray(SortArray(arr2));
